fbddf6d674e747471cb6b9992be384ade5a1c725
Tiago Gomes <tidgomes@gmail.com>
2026-06-19 15:18:34 -0300

    create loadTasks method

--- src/TaskTracker.java
+++ src/TaskTracker.java
@@ -1,3 +1,5 @@
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
@@ -14,6 +16,27 @@ public class TaskTracker {
 
     private List<Task> loadTasks() {
         List<Task> storedTasks = new ArrayList<>();
+
+        if (!Files.exists(FILE_PATH)) {
+            return new ArrayList<>();
+        }
+
+        try {
+            String jsonContent = Files.readString(FILE_PATH);
+            String[] taskList = jsonContent.replace("[", "").replace("]", "").split("},");
+
+            for (String taskJson : taskList) {
+                if (!taskJson.endsWith("}")) {
+                    taskJson = taskJson + "}";
+                    storedTasks.add(Task.fromJson(taskJson));
+                } else {
+                    storedTasks.add(Task.fromJson(taskJson));
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         return storedTasks;
     }
 }
