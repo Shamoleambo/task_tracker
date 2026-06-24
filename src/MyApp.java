364dec936214abadbb7430b1cec2ad5de2113809
Tiago Gomes <tidgomes@gmail.com>
2026-06-24 14:42:48 -0300

    refactor MyApp and create TaskCLIApp class for the running command line program

--- src/MyApp.java
+++
@@ -1,7 +0,0 @@
-public class MyApp {
-    public static void main(String[] args) {
-        TaskManager taskTracker = new TaskManager();
-        taskTracker.deleteTask("4ac27327-ee48-4c42-9447-0a93267a404a");
-        taskTracker.quit();
-    }
-}
